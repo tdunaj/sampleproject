37679c81a499acef955b2291306b5b777510e0b4
Dunaj <TDUNAJ@wellcare.com>
2017-12-07 17:22:31 -0500

    initial commit

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { of } from 'rxjs/observable/of';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) { }
+
+  //Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      //wait 300ms after each key stroke
+      debounceTime(300),
+      distinctUntilChanged(),
+      //switch to new obserable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+//replaced with in memory data service
+
+// import { Hero } from './hero'
+
+// export class MockHeroes {
+// }
+
+// export const HEROES: Hero[] = [
+//     { id: 11, name: 'Mr. Nice' },
+//     { id: 12, name: 'Narco' },
+//     { id: 13, name: 'Bombasto' },
+//     { id: 14, name: 'Celeritas' },
+//     { id: 15, name: 'Magneta' },
+//     { id: 16, name: 'RubberMan' },
+//     { id: 17, name: 'Dynama' },
+//     { id: 18, name: 'Dr IQ' },
+//     { id: 19, name: 'Magma' },
+//     { id: 20, name: 'Tornado' }
+// ];
